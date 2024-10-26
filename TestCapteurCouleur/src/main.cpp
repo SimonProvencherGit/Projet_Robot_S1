d9d00dd172e6a78274a2bda89f5be3756c52d126
SimonProvencherGit <simonprovencher28@gmail.com>
2024-10-25 20:35:26 -0400

    code suiveur de ligne

--- TestCapteurCouleur/src/main.cpp
+++ TestCapteurCouleur/src/main.cpp
@@ -6,10 +6,29 @@
 
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
+int sifletAmbient = 35;
+int siflet5Khz = 43;
+bool depart = false;
+bool bumperArriere;
+
+bool detectSiflet(){
+ int pin35 = digitalRead(sifletAmbient);
+ int pin43 = digitalRead(siflet5Khz);
+  if(pin35 && pin43){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
 void setup() 
 {
+    BoardInit();
+    
     Serial.begin(9600);
-    Serial.println("Color View Test!");
+    //Serial.println("Color View Test!");
 
     pinMode(A0,INPUT);              //initialisation des capteurs de détecteurs de ligne
     pinMode(A1,INPUT);
@@ -19,15 +38,112 @@ void setup()
         Serial.println("Found sensor");
     } else {
         Serial.println("No TCS34725 found ... check your connections");
-        while (1); // halt!
+       // while (1); // halt!
     }
 }
 
 
-void loop(void) {
-    uint16_t r, g, b, c, colorTemp, lux;
-    bool capt1,capt2,capt3;
+void loop(void) 
+{
+    int capt1,capt2,capt3;
+    
+    int vitesse = 1;
+    uint8_t id = 3;
 
+    /*if(detectSiflet())
+    {
+        depart = true;
+        Serial.print("siflet detecte");
+    }*/
+    /*capt1 = analogRead(A0);            //lecture des capteurs de détecteurs de ligne
+    capt2 = analogRead(A1);
+    capt3 = analogRead(A2);
+    
+    if(capt1<800)
+            capt1 = 0;
+        else if(capt1>800)
+            capt1 = 1;
+        
+        if(capt2<100)
+            capt2 = 0;
+        else if(capt2>100)
+            capt2 = 1;  
+        
+        if(capt3<100)
+            capt3 = 0;
+        else if(capt3>100)
+            capt3 = 1;
+    
+    Serial.print(capt1,DEC);        //affiche les valeurs des capteurs de détecteurs de ligne
+    Serial.print("   "); 
+    Serial.print(capt2,DEC);
+    Serial.print("   ");
+    Serial.print(capt3,DEC);
+    Serial.println("   ");    
+    
+    delay(500);*/
+    bumperArriere = ROBUS_IsBumper(id);
+
+    if(bumperArriere)
+        depart = true;
+
+    if(depart)
+    {   
+        capt1 = analogRead(A0);            //lecture des capteurs de détecteurs de ligne
+        capt2 = analogRead(A1);
+        capt3 = analogRead(A2);
+
+        if(capt1<800)
+            capt1 = 0;
+        else if(capt1>800)
+            capt1 = 1;
+        
+        if(capt2<100)
+            capt2 = 0;
+        else if(capt2>100)
+            capt2 = 1;  
+        
+        if(capt3<100)
+            capt3 = 0;
+        else if(capt3>100)
+            capt3 = 1;
+        
+        if(capt1 == 0 && capt2 == 1 && capt3 == 0)
+        {
+            MOTOR_SetSpeed(RIGHT,0.3*vitesse);
+            MOTOR_SetSpeed(LEFT,0.3*vitesse);
+        }
+        else if (capt1 == 1 && capt2 == 1 && capt3 == 0 || capt1 == 1 && capt2 == 0 && capt3 == 0)
+        {
+            MOTOR_SetSpeed(RIGHT,0.3*vitesse);
+            MOTOR_SetSpeed(LEFT,0);
+        }
+        else if (capt1 == 0 && capt2 == 1 && capt3 == 1 || capt1 == 0 && capt2 == 0 && capt3 == 1)
+        {
+            MOTOR_SetSpeed(RIGHT,0);
+            MOTOR_SetSpeed(LEFT,0.3*vitesse);
+        }
+        else if(capt1 == 1 && capt2 == 1 && capt3 == 1)
+        {
+            MOTOR_SetSpeed(RIGHT,0);
+            MOTOR_SetSpeed(LEFT,0);
+        }
+        /*else if(capt1 == 0 && capt2 == 0 && capt3 == 0)
+        {
+            MOTOR_SetSpeed(RIGHT,0);
+            MOTOR_SetSpeed(LEFT,0);
+        }
+        else{
+            MOTOR_SetSpeed(RIGHT,0);
+            MOTOR_SetSpeed(LEFT,0);
+        }*/
+    }
+}
+
+
+    /*uint16_t r, g, b, c, colorTemp, lux;
+    bool capt1,capt2,capt3;
+    int lim = 100;
 
     tcs.getRawData(&r, &g, &b, &c);     //lecture des valeurs RGB
 
@@ -37,9 +153,9 @@ void loop(void) {
     //Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
     //Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
 
-    capt1 = digitalRead(A0);            //lecture des capteurs de détecteurs de ligne
-    capt2 = digitalRead(A1);
-    capt3 = digitalRead(A2);
+    capt1 = digitalRead(A7);            //lecture des capteurs de détecteurs de ligne
+    capt2 = digitalRead(A3);
+    capt3 = digitalRead(A0);
 
     Serial.print(capt1,DEC);        //affiche les valeurs des capteurs de détecteurs de ligne
     Serial.print(capt2,DEC);
@@ -56,9 +172,9 @@ void loop(void) {
     int green = g;
     int blue = b;
 
-    if(red>705 && green<815 && blue<705)    //verifie les valeurs rgb pour rouge
+    if(red>740 && green<815 && blue<705)    //verifie les valeurs rgb pour rouge
         Serial.print("Rouge identifie");
-    else if(red<750 && green>850 && blue>850)   //pour bleue
+    else if(red<650 && green>760 && blue>670)   //pour bleu
         Serial.print("Bleu identifie");
     else if(red<630 && green>800 && blue<750)    //pour vert
         Serial.print("Vert identifie");
@@ -66,5 +182,5 @@ void loop(void) {
         Serial.print("Jaune identifie");
     else
         Serial.print("Aucune couleur identifie");
-    
-}
+
+    */
