040f55b5892a2960c4ceaeb81d0c975fd5c72624
SimonProvencherGit <simonprovencher28@gmail.com>
2024-10-29 15:27:53 -0400

    nice :)

--- RobotOctogone/src/main.cpp
+++ RobotOctogone/src/main.cpp
@@ -8,11 +8,11 @@
 void tournerAngleDroit(int angle);
 void tournerAngleGauche(int angle);
 bool detectSiflet();
-void premierVirage(int couleur);
-void allerCentreDebut(int couleur);
+void premierVirage();
+void allerCentreDebut();
 void touverLigneExtremite();
 void prendreValeurSuiveur();
-int detectCouleur();
+void detectCouleur();
 void suiveurLigne();
 void TestTestTest();
 void V2();
@@ -31,9 +31,9 @@ bool depart = false;
 bool bumperArriere;
 uint8_t id = 3;
 int vitesse = 1;
-String couleur[5] = {"Aucune","Rouge","Bleu","Vert","Jaune"};
 int nbObjetsButs = 0;
 int capt1,capt2,capt3;
+int couleur = 0;
 
 void setup() {
   BoardInit();
@@ -57,59 +57,69 @@ void setup() {
 }
 
 void loop() {
-  int couleur;
-  
-  scan90Droit();
 
-  while(1);
-  //suiveurLigne();
+  //tournerAngleDroit(45);tournerAngleGauche((360-45));
+  //premierVirage(4);
+  //touverLigneExtremite();
+  
+  //scan90Gauche();
+  //scan90Droit();
+  //while(1);
   /*while(1)
   {
-    couleur = detectCouleur();
+    detectCouleur();
     Serial.println(couleur);
+    //prendreValeurSuiveur();
+    delay(300);
   }*/
+  //suiveurLigne();
+
   //V2();
   //touverLigneExtremite();
+
 
   bumperArriere = ROBUS_IsBumper(id);
 
   if(bumperArriere)
     depart = true;
 
+  
   if(depart) {
     
     while(couleur == 0)
     {
-      couleur = detectCouleur();
+      detectCouleur();
+      //Serial.println(couleur);
       delay(5);
     }
     
-    allerCentreDebut(couleur);
+    allerCentreDebut();
 
     if(nbObjetsButs == 0)   //tourner pour faire face au bon triangle
     {
       MOTOR_SetSpeed(LEFT,0);
       MOTOR_SetSpeed(RIGHT,0);
-            
-      //premierVirage(couleur);   // attention, remettre couleur dans la parenthese pas un chiffre, jsute pour tester le chiffre
-      premierVirage(1);
+
+      //while(1);     
+      premierVirage();   
+      //premierVirage(2);
       //while(1);
     }
     else
     {
       tournantSuite(nbObjetsButs);      
     }
-    MOTOR_SetSpeed(LEFT,0.3);
+    MOTOR_SetSpeed(LEFT,0.35);
     MOTOR_SetSpeed(RIGHT,0.3);
     delay(1000);
     MOTOR_SetSpeed(LEFT,0);
     MOTOR_SetSpeed(RIGHT,0);
-    
+    //while(1);
     touverLigneExtremite();
   
     MOTOR_SetSpeed(LEFT,0);
     MOTOR_SetSpeed(RIGHT,0);
-    //while(1);
+    while(1);
     //code pour trouver objet
 
     //code pour trouver la ligne sur l'extrimite et se diriger vers le but
@@ -188,59 +198,61 @@ void tournerAngleGauche(int angle)
   
 void tournerAngleDroit(int angle) 
 {
-  double seuilAngle1;
+  double seuilAngle;
 
   //calculs pour trouver la valeur des encodeurs pour l'angle voulu
   // 1905/90 = x/angle
   // x = 1905/90 * angle  = nb de pulses pour l'angle voulu
 
-  seuilAngle1 = angle * (1905/90); 
+
+  seuilAngle = angle * (1905/90); 
 
   int Md = 0;
   int Mg = 0;
   ENCODER_Reset(LEFT);
   ENCODER_Reset(RIGHT);
-  while(Md<(seuilAngle1*0.03))
+
+  while(Mg<(seuilAngle*0.03))       //courbe d'acceleration commence par petite vitesse pour le premier 3% de la rotation
   {
-    MOTOR_SetSpeed(LEFT, 0.1);
     MOTOR_SetSpeed(RIGHT, -0.1);
-    Md = ENCODER_Read(LEFT);
+    MOTOR_SetSpeed(LEFT, 0.1);
+    Mg = ENCODER_Read(LEFT);
   }
-    while(Md<(seuilAngle1 * 0.06))
+  while(Mg<(seuilAngle * 0.06))   //augmentation de la vitesse jusqu'a 6% de la rotation 
   {
-    MOTOR_SetSpeed(LEFT, 0.2);
     MOTOR_SetSpeed(RIGHT, -0.2);
-    Md = ENCODER_Read(LEFT);
+    MOTOR_SetSpeed(LEFT, 0.2);
+    Mg = ENCODER_Read(LEFT);
   }
-  while(Md<(seuilAngle1*0.94))
+  while(Mg<(seuilAngle*0.94))    //vitesse maximale jusqu'a 94% de la rotation
   {
-    MOTOR_SetSpeed(LEFT, 0.3);
-    MOTOR_SetSpeed(RIGHT, -0.3-0.00535);
-    Md = ENCODER_Read(LEFT);
+    MOTOR_SetSpeed(RIGHT, -0.3);
+    MOTOR_SetSpeed(LEFT, 0.3+0.00535);
+    Mg = ENCODER_Read(LEFT);
   }
-  while(Md<seuilAngle1)
+  while(Md<seuilAngle)        //ralentissement pour les derniers 6% de la rotation
   {
-    MOTOR_SetSpeed(LEFT, 0.1);
     MOTOR_SetSpeed(RIGHT, -0.1);
-    Md = ENCODER_Read(LEFT);
-    Mg = ENCODER_Read(RIGHT);
+    MOTOR_SetSpeed(LEFT, 0.1);
+    Md = ENCODER_Read(RIGHT);
+    Mg = ENCODER_Read(LEFT);
   }
   //Mg = Mg -1;
   Mg = -1;
-  while(Mg-Md >5)   //Tant que la difference entre les deux encodeurs est plus grande que 5 pulses
+  while(Mg-Md >5)   //Tant que la difference entre les deux encodeurs est plus grande que 5 pulses les moteurs sont pas allignés
   {
-    while(Mg>=Md)
+    while(Md>=Mg)
     {
-      MOTOR_SetSpeed(LEFT, 0.0);
-      MOTOR_SetSpeed(RIGHT, 0.1);
-      Mg = ENCODER_Read(RIGHT)*-1;
+      MOTOR_SetSpeed(RIGHT, -0.1);
+      MOTOR_SetSpeed(LEFT, 0);
+      Mg = ENCODER_Read(LEFT)*-1;
       //Mg = ENCODER_Read(LEFT);
     }
-    while(Mg<=Md)
+    while(Md<=Mg)
     {
-      MOTOR_SetSpeed(LEFT, 0.0);
-      MOTOR_SetSpeed(RIGHT, -0.1);
-      Mg = ENCODER_Read(RIGHT)*-1;
+      MOTOR_SetSpeed(RIGHT, 0.1);
+      MOTOR_SetSpeed(LEFT, 0.1);
+      Mg = ENCODER_Read(LEFT)*-1;
       //Mg = ENCODER_Read(LEFT);
     }
   }
@@ -291,6 +303,11 @@ void suiveurLigne()
         delay(1000);
         MOTOR_SetSpeed(RIGHT,0);
         MOTOR_SetSpeed(LEFT,0);
+        
+        SERVO_Enable(0);
+        SERVO_Enable(1);
+        SERVO_SetAngle(0,80);
+        SERVO_SetAngle(1,110);
 
         sort = true;  
         delay(500);
@@ -302,7 +319,7 @@ void suiveurLigne()
 }
 
 //detecte couleur  --> retourne 1 si rouge, 2 si bleu, 3 si vert, 4 si jaune, 0 si aucune couleur
-int detectCouleur()
+void detectCouleur()
 {
   uint16_t r, g, b, c;
 
@@ -311,63 +328,102 @@ int detectCouleur()
   int red = r;
   int green = g;
   int blue = b;
+  
+  int rRed = 660;
+  int gRed = 620;
+  int bRed = 677;
 
-  int rRed = 865;
-  int gRed = 853;
-  int bRed = 690;
+  int rBlue = 406;
+  int gBlue = 675;
+  int bBlue = 840;
 
-  int rBlue = 544;
-  int gBlue = 863;
-  int bBlue = 769;
+  int rGreen = 425;
+  int gGreen = 705;
+  int bGreen = 719;
 
-  int rGreen = 570;
-  int gGreen = 932;
-  int bGreen = 697;
+  int rYellow = 1100;
+  int gYellow = 1150;
+  int bYellow = 845;
 
-  int rYellow = 1343;
-  int gYellow = 1516;
-  int bYellow = 901;
+  /*int rRed = 630;
+  int gRed = 605;
+  int bRed = 658;
+
+  int rBlue = 383;
+  int gBlue = 632;
+  int bBlue = 778;
+
+  int rGreen = 399;
+  int gGreen = 665;
+  int bGreen = 676;
+
+  int rYellow = 1027;
+  int gYellow = 1074;
+  int bYellow = 797;*/
 
 
-  /*Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
   Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-  Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");*/
+  Serial.print("B: "); Serial.print(b, DEC); Serial.println(" ");
 
-  if(abs(red - rRed) < 50 && abs(green - gRed) < 50 && abs(blue - bRed) < 50)    
-    return 1;
+  if(abs(red - rRed) < 25 && abs(green - gRed) < 25 && abs(blue - bRed) < 25) 
+  {
+    couleur = 1;
+    //return 1;
+  }   
     //Serial.print("Rouge identifie");
-  else if(abs(red - rBlue)<50 && abs(green-gBlue) < 50 && abs(blue - bBlue) < 50)   //pour bleu
-    return 2;
+  else if(abs(red - rBlue) <25 && abs(green-gBlue) < 25 && abs(blue - bBlue) < 40)   //pour bleu
+    {
+      couleur = 2;
+      //return 2;
+    }
   //Serial.print("Bleu identifie");
-  else if(abs(red-rGreen) <50 && abs(green - gGreen) <50 && abs(blue - bGreen) <50)    //pour vert
-    return 3;
+  else if(abs(red-rGreen) <35 && abs(green - gGreen) <35 && abs(blue - bGreen) <35)    //pour vert
+    {
+      //return 3;
+      couleur = 3;
+    }
     //Serial.print("Vert identifie");
   else if (abs(red-rYellow) < 70 && abs(green - gYellow) < 70 && abs(blue -bYellow) <70)   //pour jaune
-    return 4;  
+    {
+      //return 4;
+      couleur = 4;  
+    }
     //Serial.print("Jaune identifie");
-  
   else
-    return 0;
+  {
+    //return 0;
+    couleur = 0;
+  }
     //Serial.print("Aucune couleur identifie");
 }
 
-void premierVirage(int couleur)
+void premierVirage()
 {
   if(couleur == 1)
   {
-    tournerAngleGauche(135);
+    //tournerAngleGauche(135);
+    tournerAngleGauche(180);
+    delay(100);
+    scan90Droit();
+
   }
   else if(couleur == 2)
   {
-    tournerAngleDroit(45);
+    //tournerAngleGauche(315);
+    scan90Droit();
   }
   else if(couleur == 3)
   {
-    tournerAngleDroit(135);
+    //tournerAngleGauche(225);
+    tournerAngleGauche(180);
+    delay(100);
+    scan90Gauche();
   }
   else if(couleur == 4)
   {
-    tournerAngleGauche(45);
+    //tournerAngleGauche(45);
+    scan90Gauche();
   }
   /*else
   {
@@ -405,7 +461,7 @@ void allerCentreSuite(int nbObjetsButs)
 
 }
 
-void allerCentreDebut(int couleur)
+void allerCentreDebut()
 {
   bool sortie = false;
   int lignedetecte = 0;
@@ -488,7 +544,7 @@ void prendreValeurSuiveur()
     capt1 = 1;
   if(capt2<=30)
     capt2 = 0;
-  else if(capt2>30)
+  else if(capt2>33)
     capt2 = 1;  
   if(capt3<100)
     capt3 = 0;
@@ -517,6 +573,11 @@ void touverLigneExtremite()      // a retravailler cas ou les 3 capteurs sont su
       MOTOR_SetSpeed(RIGHT,0.3);
 
       prendreValeurSuiveur();
+    }
+    else if(capt1 == 1 && capt3==1) 
+    {
+      tournerAngleGauche(60);
+      suiveurLigne();
     }
     else if(capt1 == 1)
     {
@@ -674,22 +735,24 @@ void scan90Gauche()
 
 void scan90Droit()
 {
-  int seuilAngle = 1905;
-  int Md = 0;
+int Md = 0;
   int Mg = 0;
+  int seuilAngle = 1905;
+
   ENCODER_Reset(LEFT);
   ENCODER_Reset(RIGHT);
-  
-  while(Md<seuilAngle)
+
+  while(Md<seuilAngle)        
   {
     MOTOR_SetSpeed(LEFT, 0.05);
     MOTOR_SetSpeed(RIGHT, -0.05);
     Md = ENCODER_Read(LEFT);
     Mg = ENCODER_Read(RIGHT);
+    //si detecte objet arrete de tourner et avance chercher l'objet
   }
   //Mg = Mg -1;
   Mg = -1;
-  while(Mg-Md >5)   //Tant que la difference entre les deux encodeurs est plus grande que 5 pulses
+  while(Mg-Md >5)   //Tant que la difference entre les deux encodeurs est plus grande que 5 pulses les moteurs sont pas allignés
   {
     while(Mg>=Md)
     {
@@ -708,5 +771,5 @@ void scan90Droit()
   }
   MOTOR_SetSpeed(RIGHT,0);
   MOTOR_SetSpeed(LEFT,0);
-  delay(100);
+  delay(175);
 }
