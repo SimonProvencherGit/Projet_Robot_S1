c333bdd673b9b32be898ce97804f6a128d95a24e
amry1 <amry5183@usherbrooke.ca>
2024-10-30 11:51:47 -0400

    Code

--- RobotOctogone/src/main.cpp
+++ RobotOctogone/src/main.cpp
@@ -1,8 +1,11 @@
 #include <Arduino.h>
 #include <LibRobus.h>
-#include <Wire.h>
 #include "Adafruit_TCS34725.h"
 #include <stdlib.h>
+#include "SharpIR.h"
+
+#define IRPin A3
+#define model 1080
 
 //declaration des fonctions
 void tournerAngleDroit(int angle);
@@ -20,6 +23,7 @@ void tournantSuite(int nbOjetsButs);
 void allerCentreSuite(int nbObjetsButs);
 void scan90Gauche();
 void scan90Droit();
+void PrendreObjet();
 
 // Déclaration des variables globales
 
@@ -34,6 +38,10 @@ int vitesse = 1;
 int nbObjetsButs = 0;
 int capt1,capt2,capt3;
 int couleur = 0;
+
+
+
+
 
 void setup() {
   BoardInit();
@@ -56,12 +64,37 @@ void setup() {
 
 }
 
+ int distance_cm;
+
+void ferme_bras()
+{
+  SERVO_Enable(1);
+  SERVO_Enable(0);
+  delay(5);
+  SERVO_SetAngle(0,115);
+  SERVO_SetAngle(1,65);
+}
+
+void getdistance()
+  {
+    SharpIR mySensor = SharpIR(IRPin, model);
+    distance_cm = mySensor.distance();
+    Serial.println(distance_cm);
+  }
+
 void loop() {
+
+//scan90Gauche();
+  PrendreObjet();
+  while(1);
+//scan90Gauche();
+  // Detection d'objet
+
 
   //tournerAngleDroit(45);tournerAngleGauche((360-45));
   //premierVirage(4);
   //touverLigneExtremite();
-  
+
   //scan90Gauche();
   //scan90Droit();
   //while(1);
@@ -83,16 +116,16 @@ void loop() {
   if(bumperArriere)
     depart = true;
 
-  
+
   if(depart) {
-    
+
     while(couleur == 0)
     {
       detectCouleur();
       //Serial.println(couleur);
       delay(5);
     }
-    
+
     allerCentreDebut();
 
     if(nbObjetsButs == 0)   //tourner pour faire face au bon triangle
@@ -100,38 +133,38 @@ void loop() {
       MOTOR_SetSpeed(LEFT,0);
       MOTOR_SetSpeed(RIGHT,0);
 
-      //while(1);     
-      premierVirage();   
+      //while(1);
+      premierVirage();
       //premierVirage(2);
       //while(1);
     }
     else
     {
-      tournantSuite(nbObjetsButs);      
+      tournantSuite(nbObjetsButs);
     }
-    MOTOR_SetSpeed(LEFT,0.35);
+    /*MOTOR_SetSpeed(LEFT,0.35);
     MOTOR_SetSpeed(RIGHT,0.3);
     delay(1000);
     MOTOR_SetSpeed(LEFT,0);
     MOTOR_SetSpeed(RIGHT,0);
     //while(1);
-    touverLigneExtremite();
-  
+    touverLigneExtremite();*/
+
     MOTOR_SetSpeed(LEFT,0);
     MOTOR_SetSpeed(RIGHT,0);
-    while(1);
+    //while(1);
     //code pour trouver objet
 
     //code pour trouver la ligne sur l'extrimite et se diriger vers le but
 
     //code pour deposer l'objet dans le but et se replacer pour etre pret a repartir au centre
-    // code doit, des qu'il voit de la couleur au lieu du blanc, reculer en pivorant sur la roue de droite pour 
+    // code doit, des qu'il voit de la couleur au lieu du blanc, reculer en pivorant sur la roue de droite pour
     //qu'il puisse avancer jusqu'a ce qu'il voit une ligne pour se rendre au centre
 
   }
 }
 
-void tournerAngleGauche(int angle) 
+void tournerAngleGauche(int angle)
 {
   double seuilAngle;
 
@@ -140,7 +173,7 @@ void tournerAngleGauche(int angle)
   // x = 1905/90 * angle  = nb de pulses pour l'angle voulu
 
 
-  seuilAngle = angle * (1905/90); 
+  seuilAngle = angle * (1905/90);
 
   int Md = 0;
   int Mg = 0;
@@ -153,7 +186,7 @@ void tournerAngleGauche(int angle)
     MOTOR_SetSpeed(LEFT, -0.1);
     Md = ENCODER_Read(RIGHT);
   }
-    while(Md<(seuilAngle * 0.06))   //augmentation de la vitesse jusqu'a 6% de la rotation 
+    while(Md<(seuilAngle * 0.06))   //augmentation de la vitesse jusqu'a 6% de la rotation
   {
     MOTOR_SetSpeed(RIGHT, 0.2);
     MOTOR_SetSpeed(LEFT, -0.2);
@@ -195,8 +228,8 @@ void tournerAngleGauche(int angle)
   MOTOR_SetSpeed(LEFT,0);
   delay(175);
 }
-  
-void tournerAngleDroit(int angle) 
+
+void tournerAngleDroit(int angle)
 {
   double seuilAngle;
 
@@ -205,7 +238,7 @@ void tournerAngleDroit(int angle)
   // x = 1905/90 * angle  = nb de pulses pour l'angle voulu
 
 
-  seuilAngle = angle * (1905/90); 
+  seuilAngle = angle * (1905/90);
 
   int Md = 0;
   int Mg = 0;
@@ -218,7 +251,7 @@ void tournerAngleDroit(int angle)
     MOTOR_SetSpeed(LEFT, 0.1);
     Mg = ENCODER_Read(LEFT);
   }
-  while(Mg<(seuilAngle * 0.06))   //augmentation de la vitesse jusqu'a 6% de la rotation 
+  while(Mg<(seuilAngle * 0.06))   //augmentation de la vitesse jusqu'a 6% de la rotation
   {
     MOTOR_SetSpeed(RIGHT, -0.2);
     MOTOR_SetSpeed(LEFT, 0.2);
@@ -280,7 +313,7 @@ void suiveurLigne()
   while(sort == false)
   {
     prendreValeurSuiveur();
-    
+
     if(capt1 == 0 && capt2 == 1 && capt3 == 0)
     {
         MOTOR_SetSpeed(RIGHT,0.3*vitesse);
@@ -303,15 +336,15 @@ void suiveurLigne()
         delay(1000);
         MOTOR_SetSpeed(RIGHT,0);
         MOTOR_SetSpeed(LEFT,0);
-        
+
         SERVO_Enable(0);
         SERVO_Enable(1);
         SERVO_SetAngle(0,80);
         SERVO_SetAngle(1,110);
 
-        sort = true;  
+        sort = true;
         delay(500);
-        //while(1); 
+        //while(1);
     }
   }
   MOTOR_SetSpeed(RIGHT,0);
@@ -328,7 +361,7 @@ void detectCouleur()
   int red = r;
   int green = g;
   int blue = b;
-  
+
   int rRed = 660;
   int gRed = 620;
   int bRed = 677;
@@ -366,11 +399,11 @@ void detectCouleur()
   Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
   Serial.print("B: "); Serial.print(b, DEC); Serial.println(" ");
 
-  if(abs(red - rRed) < 25 && abs(green - gRed) < 25 && abs(blue - bRed) < 25) 
+  if(abs(red - rRed) < 25 && abs(green - gRed) < 25 && abs(blue - bRed) < 25)
   {
     couleur = 1;
     //return 1;
-  }   
+  }
     //Serial.print("Rouge identifie");
   else if(abs(red - rBlue) <25 && abs(green-gBlue) < 25 && abs(blue - bBlue) < 40)   //pour bleu
     {
@@ -387,7 +420,7 @@ void detectCouleur()
   else if (abs(red-rYellow) < 70 && abs(green - gYellow) < 70 && abs(blue -bYellow) <70)   //pour jaune
     {
       //return 4;
-      couleur = 4;  
+      couleur = 4;
     }
     //Serial.print("Jaune identifie");
   else
@@ -467,7 +500,7 @@ void allerCentreDebut()
   int lignedetecte = 0;
 
   while(sortie == false)
-  {  
+  {
     for(int i=0; i<55;i++)
     {
       MOTOR_SetSpeed(LEFT, 0.3);
@@ -484,7 +517,7 @@ void allerCentreDebut()
       }
     }
 
-    if(lignedetecte == 0) 
+    if(lignedetecte == 0)
     {
       MOTOR_SetSpeed(LEFT,0.2);
       MOTOR_SetSpeed(RIGHT,0.2);
@@ -526,7 +559,7 @@ void allerCentreDebut()
       break;
       break;
     }
-  }  
+  }
   MOTOR_SetSpeed(LEFT,0);
   MOTOR_SetSpeed(RIGHT,0);
 }
@@ -545,22 +578,22 @@ void prendreValeurSuiveur()
   if(capt2<=30)
     capt2 = 0;
   else if(capt2>33)
-    capt2 = 1;  
+    capt2 = 1;
   if(capt3<100)
     capt3 = 0;
   else if(capt3>=100)
     capt3 = 1;
 
   Serial.print(capt1,DEC);        //affiche les valeurs des capteurs de détecteurs de ligne
-  Serial.print("   "); 
+  Serial.print("   ");
   Serial.print(capt2,DEC);
   Serial.print("   ");
   Serial.print(capt3,DEC);
-  Serial.println("   ");   
+  Serial.println("   ");
 }
 
 void touverLigneExtremite()      // a retravailler cas ou les 3 capteurs sont sur la ligne   *****************************************************************************
-{ 
+{
   bool sortie = false;
 
   while(sortie == false)
@@ -574,29 +607,29 @@ void touverLigneExtremite()      // a retravailler cas ou les 3 capteurs sont su
 
       prendreValeurSuiveur();
     }
-    else if(capt1 == 1 && capt3==1) 
+    else if(capt1 == 1 && capt3==1)
     {
       tournerAngleGauche(60);
       suiveurLigne();
     }
     else if(capt1 == 1)
     {
-      while(capt1 == 1) 
+      while(capt1 == 1)
       {
         MOTOR_SetSpeed(LEFT,0);
         MOTOR_SetSpeed(RIGHT,0.3);
-        
+
         prendreValeurSuiveur();
-        
+
       }
       while(capt1 != 1)
       {
         MOTOR_SetSpeed(LEFT,0);
         MOTOR_SetSpeed(RIGHT,0.3);
-        
+
         prendreValeurSuiveur();
       }
-      
+
       MOTOR_SetSpeed(RIGHT,0);
 
       suiveurLigne();
@@ -611,6 +644,48 @@ void touverLigneExtremite()      // a retravailler cas ou les 3 capteurs sont su
   }
 }
 
+
+int hasobject = 0;
+void PrendreObjet()
+{
+  int distance_cm;
+  SharpIR mySensor = SharpIR(IRPin, model);
+  distance_cm = mySensor.distance();
+  //delay(5);
+  while(distance_cm <= 10)
+  {
+    MOTOR_SetSpeed(RIGHT,0.3);
+    MOTOR_SetSpeed(LEFT,0.3);
+
+    SharpIR mySensor = SharpIR(IRPin, model);
+    distance_cm = mySensor.distance();
+    delay(5);
+  }
+
+  // Detection d'objet
+  while (distance_cm <= 10 && distance_cm > 6)
+  {
+    //Fermer les bras quand l'objet est a 7cm
+
+    //SERVO_Enable(0);
+    //SERVO_Enable(1);
+    hasobject = 1;
+
+    MOTOR_SetSpeed(RIGHT,0.15);
+    MOTOR_SetSpeed(LEFT,0.15);
+    delay(1000);
+
+    delay(500);
+    ferme_bras();
+
+    delay(500);
+    SERVO_Disable(0);
+    SERVO_Disable(1);
+
+    touverLigneExtremite();
+  }
+}
+
 void TestTestTest()     //code pour tester fonctionnement du capt1 suiveur le ligne   ----> fonctionne
 {
    while(1)
@@ -622,19 +697,19 @@ void TestTestTest()     //code pour tester fonctionnement du capt1 suiveur le li
 
     if(capt1 == 1)
     {
-      while(capt1 == 1) 
+      while(capt1 == 1)
       {
         MOTOR_SetSpeed(LEFT,0);
         MOTOR_SetSpeed(RIGHT,0.3);
-        
+
         prendreValeurSuiveur();
-        
+
       }
       while(capt1 != 1)
       {
         MOTOR_SetSpeed(LEFT,0);
         MOTOR_SetSpeed(RIGHT,0.3);
-        
+
         prendreValeurSuiveur();
       }
 
@@ -662,22 +737,22 @@ void V2()
     }
     else if(capt1 == 1)
     {
-      while(capt1 == 1) 
+      while(capt1 == 1)
       {
         MOTOR_SetSpeed(LEFT,0);
         MOTOR_SetSpeed(RIGHT,0.3);
-        
+
         prendreValeurSuiveur();
-        
+
       }
       while(capt1 != 1)
       {
         MOTOR_SetSpeed(LEFT,0);
         MOTOR_SetSpeed(RIGHT,0.3);
-        
+
         prendreValeurSuiveur();
       }
-      
+
       MOTOR_SetSpeed(RIGHT,0);
 
       suiveurLigne();
@@ -701,13 +776,23 @@ void scan90Gauche()
   ENCODER_Reset(LEFT);
   ENCODER_Reset(RIGHT);
 
-  while(Md<seuilAngle)        
+  while(Md<seuilAngle)
   {
+
+    getdistance();
     MOTOR_SetSpeed(RIGHT, 0.05);
     MOTOR_SetSpeed(LEFT, -0.05);
     Md = ENCODER_Read(RIGHT);
     Mg = ENCODER_Read(LEFT);
+    getdistance();
     //si detecte objet arrete de tourner et avance chercher l'objet
+    if(distance_cm<37)
+    {
+      MOTOR_SetSpeed(RIGHT,0);
+      MOTOR_SetSpeed(LEFT,0);
+      PrendreObjet();
+      break;
+    }
   }
   //Mg = Mg -1;
   Mg = -1;
@@ -742,12 +827,22 @@ int Md = 0;
   ENCODER_Reset(LEFT);
   ENCODER_Reset(RIGHT);
 
-  while(Md<seuilAngle)        
+  while(Md<seuilAngle)
   {
+
     MOTOR_SetSpeed(LEFT, 0.05);
     MOTOR_SetSpeed(RIGHT, -0.05);
     Md = ENCODER_Read(LEFT);
     Mg = ENCODER_Read(RIGHT);
+    getdistance();
+    //si detecte objet arrete de tourner et avance chercher l'objet
+    if(distance_cm<37)
+    {
+      MOTOR_SetSpeed(RIGHT,0);
+      MOTOR_SetSpeed(LEFT,0);
+      PrendreObjet();
+
+    }
     //si detecte objet arrete de tourner et avance chercher l'objet
   }
   //Mg = Mg -1;
