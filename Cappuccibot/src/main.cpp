ba851fe297d04254d680d816b7aa36e381f1b6be
SimonProvencherGit <simonprovencher28@gmail.com>
2024-11-12 10:36:40 -0500

    gabarit dernier code robot

---
+++ Cappuccibot/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <LibRobus.h>
+#include "Adafruit_TCS34725.h"
+#include <stdlib.h>
+#include "SharpIR.h"
+
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
